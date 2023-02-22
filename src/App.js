172d2d02b5f479a2574e8c4c46a4ff44ffd3b34f
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2023-02-22 09:32:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import { useState } from "react";
+import {
+  useState,
+  useRef
+} from "react"; 
+import "./App.css";
 
-export default function App() {
-  const [profile, setProfile] = useState({
-    name: "Lamphrangmi",
-    age: 20,
-    job: "Junior Software engineer",
-    company: "startup",
-  });
+function App() { 
+  const inputRef = useRef(null); 
+  const resultRef = useRef(null); 
+  const [result, setResult] = useState(0);
+ 
+  function plus(e) { 
+    e.preventDefault(); 
+    setResult((result) => result + Number(inputRef.current.value)); 
+  }; 
+ 
+  function minus(e) { 
+  	// Add the code for the minus function
+    e.preventDefault(); 
+    setResult((result) => result - Number(inputRef.current.value)); 
+  };
+ 
+  function times(e) { 
+    // Add the code for the plus function
+    e.preventDefault(); 
+    setResult((result) => result * Number(inputRef.current.value)); 
+  }; 
+ 
+  function divide(e) { 
+    // Add the code for the divide function
+    e.preventDefault(); 
+    setResult((result) => result / Number(inputRef.current.value)); 
+  };
+ 
+  function resetInput(e) { 
+    // Add the code for the resetInput function 
+    e.preventDefault(); 
+    inputRef.current.value = 0;
+  }; 
+ 
+  function resetResult(e) { 
+  	// Add the code for the resetResult function 
+    e.preventDefault();
+    setResult(0); 
 
-function handleChange(e) {
-  setProfile({
-    ...profile,
-    name: e.target.value,
-  })
-}
-
-  return (
-    <>
-      <label>Enter your name:
-        <input value={profile.name} onChange={handleChange} />
-      </label>
-
-      <h2>Hello my name is {profile.name}. I'm {profile.age} years old and I'm working as a {profile.job}</h2>
-    </>
-  )
-
-}
+  }; 
+ 
+  return ( 
+    <div className="App"> 
+      <div> 
+        <h1>Simplest Working Calculator</h1> 
+      </div> 
+      <form> 
+        <p ref={resultRef}> 
+          {result} 
+        </p> 
+        <input
+          pattern="[0-9]" 
+          ref={inputRef} 
+          type="number" 
+          placeholder="Type a number" 
+        /> 
+        <button onClick={plus}>add</button> 
+        <button onClick={minus}>minus</button>
+        <button onClick={times}>times</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetResult}>Reset Result</button>
+      </form> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
